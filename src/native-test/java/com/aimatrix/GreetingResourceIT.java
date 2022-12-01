894d9c1b99e06d5fb8792204b87cb1a6c9d586ac
Duc Luu <ducluuthuan@gmail.com>
2022-12-01 21:46:03 +0700

    quarkus-resteasy-reactive

---
+++ src/native-test/java/com/aimatrix/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.aimatrix;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
